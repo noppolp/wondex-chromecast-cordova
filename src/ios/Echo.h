667373252a2c6cdd1b32251f8694592b1b848c51
noppolp <knookcs08@gmail.com>
2014-10-16 13:31:04 +0700

    first test

--- src/ios/Echo.h
+++ src/ios/Echo.h
@@ -1,8 +1,13 @@
 /********* Echo.h Cordova Plugin Header *******/
 
 #import <Cordova/CDV.h>
+#import <GoogleCast/GoogleCast.h>
 
-@interface Echo : CDVPlugin
+@interface Echo : CDVPlugin<GCKDeviceScannerListener, GCKDeviceManagerDelegate, GCKMediaControlChannelDelegate>
+
+@property(nonatomic, strong) GCKDeviceScanner* deviceScanner;
+@property(nonatomic, strong) GCKDeviceManager* deviceManager;
+@property(nonatomic, readonly) GCKMediaInformation* mediaInformation;
 
 - (void)echo:(CDVInvokedUrlCommand*)command;
 
--- src/ios/Echo.m
+++ src/ios/Echo.m
@@ -19,4 +19,28 @@
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
+- (void)initCast:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+
+    self.deviceScanner = [[GCKDeviceScanner alloc] init];
+    [self.deviceScanner addListener:self];
+    [self.deviceScanner startScan];
+
+    NSString* result = @"yeah!";
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+#pragma mark - GCKDeviceScannerListener
+- (void)deviceDidComeOnline:(GCKDevice *)device {
+  NSLog(@"device found!! %@", device.friendlyName);
+  [self updateButtonStates];
+}
+
+- (void)deviceDidGoOffline:(GCKDevice *)device {
+  [self updateButtonStates];
+}
+
 @end
